bb20eecf461c28f9869d78d718910d85281a3079
Kaue <mendoncakau@gmail.com>
2023-05-17 13:59:31 -0300

    al

--- src/main/java/org/example/TesteControleAcesso.java
+++ src/main/java/org/example/TesteControleAcesso.java
@@ -6,15 +6,12 @@ public class TesteControleAcesso {
 
     public static void main(String[] args) {
         ControleAcesso controleAcesso = new ControleAcesso();
-        Scanner scanner = new Scanner(System.in);
 
         int[] codigos = { 123, 456, 789, 987, 654 }; // Valores fixos para os códigos
 
         for (int codigo : codigos) {
             controleAcesso.registrarAcesso(codigo);
         }
-
-        scanner.close();
     }
 
 }
