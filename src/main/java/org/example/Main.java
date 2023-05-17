539d87d0974a0ae030021c183b537fd542c7cd0a
Kaue <mendoncakau@gmail.com>
2023-05-17 13:23:47 -0300

    al

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,24 +7,37 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         ControleAcesso controle = new ControleAcesso();
 
+        int numIteracoes = 0;
         for (int i = 0; i < 5; i++) {
             try {
                 System.out.print("Digite o código do crachá: ");
                 int codigo;
-                String input = scanner.nextLine();
+                if (scanner.hasNextLine()) {
+                    String input = scanner.nextLine();
 
-                if (input.equalsIgnoreCase("sair")) {
-                    System.out.println("Encerrando o programa...");
-                    break;
+                    if (input.equalsIgnoreCase("sair")) {
+                        System.out.println("Encerrando o programa...");
+                        break;
+                    }
+
+                    codigo = Integer.parseInt(input);
+                    controle.registrarAcesso(codigo);
+                    numIteracoes++;
+
+                    if (numIteracoes >= 5) {
+                        System.out.println("Número máximo de iterações atingido. Encerrando o programa...");
+                        break;
+                    }
+                } else {
+                    System.out.println("Entrada inválida. Tente novamente.");
+                    i--;
                 }
-
-                codigo = Integer.parseInt(input);
-                controle.registrarAcesso(codigo);
             } catch (NumberFormatException e) {
                 System.out.println("Código inválido. Tente novamente.");
                 i--;
             }
         }
+
 
         scanner.close();
     }
