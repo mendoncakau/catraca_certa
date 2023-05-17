5d2e90e9fbdb0fc7ce4a4c7eb0eaed704c66e5d7
Kaue <mendoncakau@gmail.com>
2023-05-17 13:14:11 -0300

    al

--- src/main/java/org/example/TesteControleAcesso.java
+++ src/main/java/org/example/TesteControleAcesso.java
@@ -5,25 +5,12 @@ import java.util.ArrayList;
 public class TesteControleAcesso {
 
     public static void main(String[] args) {
-        ControleAcesso controleAcesso = new ControleAcesso();
+        ControleAcesso controle = new ControleAcesso();
 
-        // Simula valores de entrada
-        String[] valoresEntrada = {"1", "2", "3", "sair"};
+        int[] codigos = { 123, 456, 789, 123, 789, 456 };
 
-        System.out.println("Bem-vindo ao Teste do Controle de Acesso");
-
-        for (String valor : valoresEntrada) {
-            if (valor.equalsIgnoreCase("sair")) {
-                System.out.println("Encerrando o teste...");
-                break;
-            }
-
-            try {
-                int codigo = Integer.parseInt(valor);
-                controleAcesso.registrarAcesso(codigo);
-            } catch (NumberFormatException e) {
-                System.out.println("Código inválido. Tente novamente.");
-            }
+        for (int codigo : codigos) {
+            controle.registrarAcesso(codigo);
         }
     }
 }
